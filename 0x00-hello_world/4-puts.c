e8ae2c73b39be6e8822be41c3df55e8b49684aa1
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-10 13:13:28 -0800

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-
- * main - Entry Point
+ * main - Entry point
  *
- *Return: Zero
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
