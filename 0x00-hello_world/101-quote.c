0aa52e2008b1f475ef0ab1afdb683e20b823696e
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-10 14:12:37 -0800

    quotes fic max char

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	char var_text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, var_text, 59);
+	write(2, txt, 59);
 	return (1);
 }
