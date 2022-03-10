0f5964de75f7e5c36669f382eaf8bf000d498675
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-10 14:09:17 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <unistd.h>
+
 /**
- * main - char string Empty Array 
- * Return: 1 
+ * main - variable char named var_text
+ * prints 2, { varible we declaired before }, 59
+ * Return: 1
  */
+
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char var_text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);
+	write(2, var_text, 59);
 	return (1);
 }
