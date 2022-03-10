33dac7cd2821b7c547d24019cb70c117f33fe22c
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-10 13:25:03 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string
+ * but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
