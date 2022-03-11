bbbae5c63cf416c1c21eb430ad26ebb32f145c3f
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-11 14:07:46 -0800

    upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char alpabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
