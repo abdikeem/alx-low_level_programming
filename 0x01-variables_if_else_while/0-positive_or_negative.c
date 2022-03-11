b35c305367e0ca6f0b71b138dee6dc6cdea42f5a
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-11 12:05:48 -0800

    rand int

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,12 @@ int main(void)
 
 	int n;
 	srand(time(0));
-	
-	n = rand();
+
+	n = rand() - RAND_MAX / 2;
 	if(n > 0){
 		printf("is positive");
 	}
-	else if(n = 0){
+	else if(n == 0){
 		printf("is zero");
 	}
 	else{
