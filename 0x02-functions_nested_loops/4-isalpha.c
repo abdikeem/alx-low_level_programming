dbfd09a96313739939f2c08ede988093cac63c0a
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 22:14:02 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
- * Description: the character to check
+ * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _isalpha(int c)
