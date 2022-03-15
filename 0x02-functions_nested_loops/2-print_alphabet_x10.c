14af9397d63317043d2afb89221ce760ab497e98
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 21:33:03 +0300

    alphabet multiply * 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -20,3 +20,26 @@ ten_times++;
 _putchar('\n');   
 }
 }
+
+#include "main.h"
+/**
+ *print_alphabet*10 - print the lowercase alphabets ten times 
+ * 
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char lower_alphabet;
+    int i = 0;
+    while (i < 10)
+    {
+        for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
+        {
+            _putchar(lower_alphabet);
+        }
+        i++;
+        _putchar('\n');
+    }
+
+}
