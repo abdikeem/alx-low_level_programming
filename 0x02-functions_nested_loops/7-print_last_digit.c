1e1697b5eb72ab2757b290682ece18292103e099
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-16 00:30:31 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,6 @@
 /**
  * int print_last_digit(int)- print last digit of number
  * 
- *
  * @c - i % 10 gives the reminder of any number greater than 10
  * Return: 0
  */
@@ -11,9 +10,13 @@
 int print_last_digit(int i)
 {
 int last_digit;
+print_last_digit(98);
+print_last_digit(0);
+last_digit = print_last_digit(-1024);
 while (i >= 10)
 /* code */
 last_digit = i % 10;
-_putchar(last_digit + "0");
+_putchar(last_digit + '0');
+_putchar('\n');
 return(0);
 }
