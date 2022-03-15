138ce73af2ff9ecc69612903df9b011c1a1b1abf
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 14:45:24 +0300

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main- Print string _putchar as the output
+ *
+ *Return: 0;
+ */
+ 
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+## Author
+Abdihakim Abdirahman
+
+## Program
+ Alx
