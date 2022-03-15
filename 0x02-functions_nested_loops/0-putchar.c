e73e931bd779f4cdc63609d5c3da0b03da23bc54
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 16:13:15 +0300

    Fix bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,14 @@
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
