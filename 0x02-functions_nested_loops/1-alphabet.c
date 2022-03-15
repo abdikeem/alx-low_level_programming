d7f9b8b07574964aaa83ebe9f1c0b60f39c03367
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 20:51:24 +0300

    fix error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,10 @@
 
 void print_alphabet(void)
 {
-char lower_alphabet;
-for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
-{
-_putchar(lower_alphabet);
-}
-_putchar('\n');
-
+    char lower_alphabet;
+    for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
+    {
+        _putchar(lower_alphabet);
+    }
+    _putchar('\n');
 }
