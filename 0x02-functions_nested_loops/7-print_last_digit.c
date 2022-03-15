8573c08678fa7a259d2bd1baa1d7d028016903e2
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 23:22:17 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int print_last_digit(int)- print last digit of number
+ * 
+ *
+ * @c - i % 10 gives the reminder of any number greater than 10
+ * Return: 0
+ */
+
+int print_last_digit(int i)
+{
+    while (i >= 10)
+    {
+        /* code */
+        return(i = i % 10);
+    }
+    
+}
+
