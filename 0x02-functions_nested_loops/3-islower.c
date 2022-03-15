449a3782eb73d74cbad8f59dd465884d04149a79
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 22:01:37 +0300

    if statement

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - returns 1 if alphabet letter is in lowercase and zero if in uppercase
+ * int _islower(int c)- returns 1 if alphabet letter is in lowercase
+ *  and zero if in uppercase
  * 
  * Return: 0
  */
 
 int _islower(int c)
 {
-
     if (c >= 97 && c <= 122)
     {
         return (1);
