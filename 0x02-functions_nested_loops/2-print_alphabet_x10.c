f200195b28c3a1be34cfd1c254a430c6a718e0de
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-15 21:28:12 +0300

    alphabet multiply * 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	char lower_alphabet;
-    int ten_times = 0;
-    while(ten_times < 10)
-    {
-        for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
-	    {
-		    _putchar(lower_alphabet);
-	    }
-        ten_times++;
-	    _putchar('\n');   
-    }
+char lower_alphabet;
+int ten_times = 0;
+while (ten_times < 10)
+{
+for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
+{
+    _putchar(lower_alphabet);
+}
+ten_times++;
+_putchar('\n');   
+}
 }
