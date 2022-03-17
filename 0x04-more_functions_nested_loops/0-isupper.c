6cf2b21a13a062b3536e2135293c2db2a38d1023
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-17 19:24:13 +0300

    digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,17 @@
 /**
  * _isupper(int c) - checks the code and prints upper case letters.
  *@c: if statement.
- *
  * Return: 1 when is uppercase and 0 when is lowercase.
  */
 
 int _isupper(int c)
 {
     if (c >= 65 && c <= 90)
-        return (1);
+    {
+    return (1);
+    }
     else
-        return (0);
+    {
+    return (0);
+    }
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 /**
- *  _isdigit(int c)- this function checks if its an number
+ * _isdigit(int c)- checks if its an number
  * 
- * @c - if its number return 1 else 0
+ * @c: if its number return 1 else 0
  * Return: 1 if number else 0
  */
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
+    if ( c >= 48 && c <= 57)
+    {
         return (1);
+    }
     else
+    {
         return (0);
+    }
 }
