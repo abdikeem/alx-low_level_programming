8cc1f2acb7f98ff3a1c7c834316afc3bab303e14
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-18 00:25:33 +0300

    for loop

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,5 +8,6 @@
 
 int mul(int a, int b)
 {
-return(a*b);
+    int c = a*b;
+    return(c);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper- function that prints number 0 - 9
+ * @c: for loop
+ * Return: return 0
+ */
+
+void print_numbers(void)
+{
+    int i;
+    for (i = 48; i <= 57; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+    return (0);
+}
