71d03a33094825880de923d96a0fc31ffc0638cf
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-18 01:00:03 +0300

    exclude 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,20 @@
 
 /**
  * _isupper- function that prints number 0 - 9
- * @c: for loop
+ * @c: without 2 and 4
  * Return: return 0
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
     int i;
-    
+
     for (i = 48; i <= 57; i++)
     {
-        _putchar(i);
+        if ( i != 2 && i != 4)
+        {
+            _putchar(i);
+        }
     }
     _putchar('\n');
 }
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
