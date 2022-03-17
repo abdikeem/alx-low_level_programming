a6b856c04cae9dfa2ea89aa7c3ca1a4c36b712ed
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-17 23:20:09 +0300

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * int _isupper(int)- function checks if its uppercase letter
+ * @c: if Uppercase return 1 else 0 
+ * Return: return 1 if uppercase else 0
+ */
+
+int _isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
