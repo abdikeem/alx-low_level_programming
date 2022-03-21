ac4006cb1a2022cf690cb17b9c8c32b3019944d9
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 22:07:47 +0300

    rev string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts - prints every later in a string
+ * 
+ * @str: string pointer
+ */
+
+void puts2(char *str)
+{
+    int i;
+    for (i = 0; str[i] = '\n'; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+}
