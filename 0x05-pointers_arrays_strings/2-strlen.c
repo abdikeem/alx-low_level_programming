582118bc160cafd6f89ed685b207ff4204653123
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 20:23:11 +0300

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * @brief int _strlen - function that returns length of a String
+ * @author Abdihakim Abdirahman
+ * 
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+int len;
+for (len = 0; len != '\0'; ++len)
+{
+    return (len);
+}
+}
