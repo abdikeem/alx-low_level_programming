5e7cc5a1d221e06e7eb9fad6d23de459cfe2ae07
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 22:34:20 +0300

    even number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
 /**
- * puts - prints every later in a string
- * 
- * @str: string pointer
- */
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
 
 void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '0')
+while (str[i] != 0)
 {
-if (i % 2 == 0)
-putchar(str[i]);
-i++;
+	if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+	}
+
+	i++;
 }
-putchar('\n');
+_putchar('\n');
 }
