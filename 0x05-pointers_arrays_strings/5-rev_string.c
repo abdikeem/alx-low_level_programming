e4e3b7b33bbf82a54f51838bd1b8e72903c70ae4
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 22:01:48 +0300

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,23 @@
  */
 void rev_string(char *s)
 {
-char rev;
-int i, len, len1;
-len1 = 0;
-for (len = 0; s[len] != '\0'; len++)
-{
-len1 = len - 1;
-for (i = 0; i < len / 2; i++)
-{
-rev = s[i];
-s[i] = s[len1];
-s[len1--] = rev;
-}
+	char rev;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		rev = s[i];
+		s[i] = s[len1];
+		s[len1--] = rev;
+	}
 }
