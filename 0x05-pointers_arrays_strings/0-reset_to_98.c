4197c77e1116fb0119e0a6afae8c89302484c239
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 18:09:12 +0300

    check errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * @brief reset_to_98(int *n) - takes in pointer to an int and updates it to 98 
+ * reset_to_98 - takes in pointer to an int and updates it to 98
  * @author Abdihakim Abdirahman
- * 
+ *
  * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-int x; 
+int x;
 x = 98;
 *n = x;
 }
