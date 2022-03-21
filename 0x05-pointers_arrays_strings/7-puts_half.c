0a16faca9217c7b34019a5ef5ee70baf5f0bd45c
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-21 22:49:29 +0300

    even number

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars, print (length - 1) / 2
+ */
+
+void puts_half(char *str)
+{
+    int i, n;
+
+if (i % 2 == 1)
+i = n / 2;
+else
+i = (i - 1) / 2;
+
+for (i = 0; i < n; i++)
+{
+    putchar(str[i]);
+}
+putchar('\n');
+}
