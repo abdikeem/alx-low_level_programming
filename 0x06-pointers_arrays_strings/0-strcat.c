e25287cc8d5011ec3e63d21c6359b8f8b346b109
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-23 20:15:09 +0300

    fix bug quiz 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,11 @@ char *_strcat(char *dest, char *src)
 int i,j;
 
 for (i = 0; dest[i] != '\0'; i++);
-for (j = 0; src[j] != '\0'; j++)
+while (src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
+j++;
 }
 dest[i] = '\0';
 return (dest);
