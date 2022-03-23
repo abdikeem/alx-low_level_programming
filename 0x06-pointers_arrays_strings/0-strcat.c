4e3ac176d606a8386f2723dda2cf563bd4ee3a77
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-23 18:26:50 +0300

    concatinate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,8 @@
 
 /**
  *_strcat- function that concatenates two strings
- * 
+ * @dest: return value
+ * @src: source
  *Return: pointer to the resulting string dust
  */
 
@@ -12,8 +13,8 @@ int i,j;
 for (i = 0; dest[i] != '\0'; i++);
 for (j = 0; src[j] != '\0'; j++)
 {
-    dest[i] = src[j];
-    i++;
+dest[i] = src[j];
+i++;
 }
 dest[i] = '\0';
 return (dest);
