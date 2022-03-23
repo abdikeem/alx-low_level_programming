bb4ef7d45c84ffc1a51a9b7930b30b23d0dbe32e
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-23 20:28:16 +0300

    fix error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-int i,j;
+int i = 0,j;
 
-for (i = 0; dest[i] != '\0'; i++)
+/* for (i = 0; dest[i] != '\0'; i++) */
+while (dest[i] != '\0')
+{
+i++;
+}
 for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j]; 
