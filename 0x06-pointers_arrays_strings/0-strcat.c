6af805c7368a6878f526fa56403bc888ab0969f5
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-23 18:12:42 +0300

    concatinate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strcat- function that concatenates two strings
+ * 
+ *Return: pointer to the resulting string dust
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i,j;
+for (i = 0; dest[i] != '\0'; i++);
+for (j = 0; src[j] != '\0'; j++)
+{
+    dest[i] = src[j];
+    i++;
+}
+dest[i] = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+#endif
