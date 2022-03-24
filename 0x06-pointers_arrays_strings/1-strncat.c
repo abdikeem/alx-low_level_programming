f2574b0f156168c24683517d2b1ac74de75456a2
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 10:02:48 +0300

    strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i,j;
 
-for (i = 0; dest[i] != '\0'; i++);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 for (j = 0; j < n; j++)
 {
 dest[i] = src[j];
