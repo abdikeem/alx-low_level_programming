591612dabf2ea50490f7bf08df0ef62a7bfa2d31
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 12:53:13 +0300

    reverse_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,6 +2,7 @@
 
 /**
  * reverse_array - returns array backwards
+ * @a: pointer
  * @n: number of elements of the array
  * Return: 0
  */
@@ -10,7 +11,7 @@ void reverse_array(int *a, int n)
 {
 int i , x, rev;
 
-for (i = 0; i < n; i++)
+for (i = 0; i < n - 1; i++)
 {
 for (x = i + 1; x > 0; x--)
 {
@@ -20,4 +21,3 @@ rev = *(a + x);
 }
 }
 }
-
