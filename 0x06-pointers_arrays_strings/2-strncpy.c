81d373181c9db0f57091a11aaabee9828c9f6c57
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 10:35:57 +0300

    fix bug

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
  * strncpy - function that copies string
  * @dest: destination string
  * @src: source string
+ * @n: number of charactors
  * @author: Abdihakim Abdirahman
  * Return: dest
  */
@@ -13,7 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
+{
 dest[i] = src[i];
+}
 while (i < n)
 {
 dest[i] = '\0';
