1e41f74af8372542be1846171bce026e42271aec
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-25 00:17:57 +0300

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,24 +7,24 @@
  */
 char *cap_string(char *k)
 {
-	int j = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+int j = 0, i;
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (k[j] >= 97 && k[j] <= 122)
-		k[j] = k[j] - 32;
-	j++;
-	while (k[j] != '\0')
-	{
-		for (i = 0; i < 13; i++)
-		{
-			if (k[j] == sep_words[i])
-			{
-				if ((k[j + 1]) >= 97) && (k[j + 1]) <= 122))
-					k[j + 1]) = s[j + 1]) - 32;
-				break;
-			}
-		}
-		j++;
-	}
-	return (k);
+if (k[j] >= 97 && k[j] <= 122)
+k[j] = k[j] - 32;
+j++;
+while (k[j] != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (k[j] == sep_words[i])
+{
+if ((k[j + 1]) >= 97) && (k[j + 1]) <= 122))
+k[j + 1]) = s[j + 1]) - 32;
+break;
+}
+}
+j++;
+}
+return (k);
 }
