b980bb88d0428dd8f5d6075806b3199d302fe843
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 12:34:32 +0300

    reverse_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,17 +8,13 @@
 
 void reverse_array(int *a, int n)
 {
-int i = 0, x, rev;
+int i , x, rev;
 
-while (i < n)
-{
-/* code */
-i++;
+for (i = 0; i < n; i++)
 for (x = i + 1; x > 0; x--)
 {
 rev = *(a + x);
 *(a + x) = *(a + (x - 1));
 *(a + (x - 1)) = rev;
-}
 }
 }
