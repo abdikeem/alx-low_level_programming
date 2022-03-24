0be9a8bdec9285a0d0c65151b3c6de8fa8b493a5
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 23:04:54 +0300

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes lowercase to uppercase
+ * @k: string
+ * Return: return k
+ */
+
+char *string_toupper(char *k)
+{
+int i;
+
+for (i = 0; i < k; i++)
+{
+    if (k[i] >= 'a' && k[i] <= 'z')
+    k[i] = k[i] - 32;
+    else if (k[i] >= 'A' && k[i] <= 'Z')
+    k[i] = k[i] + 32;
+}
+}
