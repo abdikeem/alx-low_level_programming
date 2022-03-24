faf843ede016e4e495169a434044a7f2f5597a96
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 10:23:42 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strncpy - function that copies string
+ * @dest: destination string
+ * @src: source string
+ * @author: Abdihakim Abdirahman
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (i = 0; i < n ; i++)
+dest[i] = '\0';
+return (dest);
+}
