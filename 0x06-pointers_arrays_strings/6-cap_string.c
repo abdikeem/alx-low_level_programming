670dd52e2493244b555e30ff1224ce783f827464
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-25 00:21:10 +0300

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,36 @@
 #include "main.h"
-
 /**
- * *cap_string - capitalize all string words
- * @k: input string
- * Return: k
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-char *cap_string(char *k)
+char *cap_string(char *s)
 {
-int j = 0, i;
-int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count;
 
-if (k[j] >= 97 && k[j] <= 122)
-k[j] = k[j] - 32;
-j++;
-while (k[j] != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if (k[j] == sep_words[i])
-{
-if ((k[j + 1]) >= 97) && (k[j + 1]) <= 122))
-k[j + 1]) = s[j + 1]) - 32;
-break;
-}
-}
-j++;
-}
-return (k);
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
