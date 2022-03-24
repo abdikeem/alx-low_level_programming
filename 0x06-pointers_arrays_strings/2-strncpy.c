5be65dfe85c5f08da36ee72ad8fba8b340e5843d
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 10:31:32 +0300

    fix error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
+
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
-for (i = 0; i < n ; i++)
+for ( ; i < n ; i++)
 dest[i] = '\0';
 return (dest);
 }
