44536e03dfabe5f4fdc544d24a4421691939bcb4
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 10:09:33 +0300

    fix bug

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i,j;
+int i = 0;
+int j = 0;
 
-i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-for (j = 0; j < n; j++,i++;)
+while (j < n)
 {
 dest[i] = src[j];
 if (src[j] == '\0')
 break;
+i++;
+j++;
 }
 return (dest);
 }
