198bf4291c1ad0142ae7891a405df7ae8ae2447d
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 11:11:46 +0300

    strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,10 @@ int _strcmp(char *s1, char *s2)
 int i = 0;
 while (s1[i] == s2[i])
 {
-if (s1[i] == '\0' || s2[i] == '\0')
+if (s1[i] == '\0' && s2[i] == '\0')
 {
     break;
 i++;
 }
-if (s1[i] == '\0' && s2[i] == '\n')
-return (0);
-else
-return (-1);
 }
 }
