56c2b593fb492db84d8d08c9db2d4edad9fc3718
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-24 11:19:11 +0300

    strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,13 +2,17 @@
 
 /**
  * _strcmp - function that compares two string
- * Return: 0 or -1
+ * @s1: pointer one
+ * @s2: pointer two
+ * Return: j when s1 == s2
+ * else nothing
  */
 
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 int j = 0;
+
 while (j == 0)
 {
 if (s1[i] == '\0' && s2[i] == '\0')
@@ -17,6 +21,6 @@ break;
 j = s1[i] - s2[i];
 i++;
 }
-return (j);
 }
+return (j);
 }
