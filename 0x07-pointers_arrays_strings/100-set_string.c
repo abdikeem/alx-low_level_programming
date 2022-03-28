b44f380a54a0476b2768f32c8f350ce9e8c47d03
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-28 23:29:52 +0300

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - redirect.
+ * @s: double pointer to address
+ * @to: the original pointer
+ *
+ * Return: chessboard.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
