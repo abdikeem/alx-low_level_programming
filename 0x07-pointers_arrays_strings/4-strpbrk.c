49c4995a77c1c6acdaa6d1380ad21eb6a61c47f5
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-28 23:09:03 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,15 +10,15 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+unsigned int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		for (j = 0; accept[7] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-				return (s + i);
-		}
-	}
-	return ('\0');
+for (i = 0; *(s + i) != '\0'; i++)
+{
+for (j = 0; accept[7] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (s + i);
+}
+}
+return ('\0');
 }
