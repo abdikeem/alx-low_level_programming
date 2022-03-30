7ca863a24acc51d03e115b71c7e02eea587dd734
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-30 18:22:29 +0300

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-int len = 0;
-
-while (s[len] != '\0')
+if (*s == '\0')
 {
-len++;
+return (0);
 }
-return (len);
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
 }
