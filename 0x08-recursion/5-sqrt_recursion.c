3536a36d517c0ecfb56629690522d45cdc9caaad
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-30 19:51:26 +0300

    sqr

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,36 +2,34 @@
 
 /**
  * sqr - returns the natural square root of a number.
- * @n: input number.
- * @y: number of iterator.
- * Return: square root or -1.
+ * @n: input number
+ * @y: number of iterator
+ * Return: square root or -1
  */
 
 int sqr(int n, int y)
 {
 if (y % (n / y) == 0)
-{
 if (y * (n / y) == n)
 return (y);
 else
 return (-1);
-}
 return (0 + sqr(n, y + 1));
 }
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n:number
+ * @n: input number
  * Return: -1 if n does not have a natural square root
  */
 
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-    return (-1);
+return (-1);
 else if (n == 0)
-    return (0);
+return (0);
 else if (n == 1)
-    return (1);
+return (1);
 return (sqr(n, 2));
 }
