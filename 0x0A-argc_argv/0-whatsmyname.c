ca32d70a4455334adce6db10a77d732532ee8d46
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-01 15:56:33 +0300

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints the program name
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char* argv[])
+{
+printf("%s",argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,15 @@
+# argc and argv[]
+
+## Technology
+compiled on Ubuntu 20.04 LTS using gcc
+Betty style
+Allowed editors: vi, vim, emacs
+
+### Program
+Alx Software Engineering
+
+#### Author
+Abdihakim Abdirahman
+
+##### License
+MIT- license
