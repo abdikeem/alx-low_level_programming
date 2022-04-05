32c309a661710839eccb4d900f4c93b62acd117b
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-05 06:11:50 +0300

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strdup - function returns a pointer to a new string which is a duplicate of the string str.
+ * 
+ * @str: duplicated string.
+ * Return: NULL if str = NULL else pointer to the duplicated string.
+ */
+
+char *_strdup(char *str)
+{
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
