5045b4c1ec987e8288c370cbdc33b157cce572a6
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-11 17:00:42 +0300

    preprocessor

--- 0x0D-preprocessor/0-object_like_macro.c
+++ 0x0D-preprocessor/0-object_like_macro.c
@@ -1,5 +1,3 @@
 #ifndef SIZE
-#define SIZE
 #define SIZE 1024
-
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,5 @@
 #ifndef ABS_H
 #define ABS_H
-
-#define ABS(X) ((x) < 0 ? -(x) : (x))
+#define ABS(X) ((x) < 0 ? (-x) : (x))
 
 #endif
