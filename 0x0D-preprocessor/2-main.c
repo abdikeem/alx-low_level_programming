8694885841dd289bcc22ea00052b3e9190848b4c
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-11 09:39:07 +0300

    path of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line.
+ * 
+ * Return: 0.
+ */
+
+int main()
+{
+printf("%s\n", __FILE__);
+return (0);
+}
