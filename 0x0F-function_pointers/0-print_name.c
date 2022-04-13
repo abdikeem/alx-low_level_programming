a54b226863941dbc2ea543286afb40e84946a927
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-13 23:50:22 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,18 +3,14 @@
 /**
  * print_name_as_is - prints a name as is
  * @name: name of the person
- * @f: pointer to name
+ * @f: void function
  * 
  * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
-else
+if (name && f)
 {
 f(name);
 }
