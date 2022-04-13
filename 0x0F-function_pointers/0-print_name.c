9c28d66e16b0d3506802f8a60f41f0b4443c5d7b
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-14 00:08:29 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_name - prints a name
+ * @name: name's main
  * @f: void function
- * 
- * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-{
-f(name);
-}
+	if (name && f)
+	{
+		f(name);
+	}
 }
