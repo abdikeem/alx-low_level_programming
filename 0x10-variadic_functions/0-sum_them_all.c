e338809bcc8409bbdbb5daf1a2746d899e29b515
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-14 23:30:44 +0300

    README.md

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all param
+ * @n:  number of param
+ * 
+ * @return sum 
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+    int sum = 0;
+    unsigned int i = 0;
+
+    va_list valuelist;
+
+    va_start(valuelist, n);
+
+    while (i < n)
+    {
+        sum += va_arg(valuelist, int);
+        i++;
+    }
+
+    va_end(valuelist);
+    return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTION_
+#define VARIADIC_FUNTION_
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif //VARIADIC FUNCTION
