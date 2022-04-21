287279a265c5cf792d793fffbf1274da3f574638
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-21 23:02:32 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,8 +6,6 @@
 #include <stddef.h>
 #include <string.h>
 
-
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -26,5 +24,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
