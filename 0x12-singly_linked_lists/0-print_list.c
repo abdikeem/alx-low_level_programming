470fc69599bb948bfccd16eccabebc4e8232bcfe
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-21 22:51:10 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+size_t nodes = 0;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
+else
+printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
-		h = h->next;
-	}
+nodes++;
+h = h->next;
+}
 
-	return (nodes);
+return (nodes);
 }
