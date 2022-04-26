fe61ed3bf59f1eb29eb1ce687b20b0fb3acb8ccb
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-26 21:15:33 +0300

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,31 +8,31 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t len = 0;
-int diff;
-listint_t *temp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-if (!h || !*h)
-return (0);
+	if (!h || !*h)
+		return (0);
 
-while (*h)
-{
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-temp = (*h)->next;
-*h = temp;
-len++;
-}
-else
-{
-*h = NULL;
-len++;
-break;
-}
-}
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
 
-*h = NULL;
+	*h = NULL;
 
-return (len);
+	return (len);
 }
