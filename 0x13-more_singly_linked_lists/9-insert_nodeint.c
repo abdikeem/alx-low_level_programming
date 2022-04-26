027fb7bd1bd67a60fb46751f5849b26d88821eea
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-26 20:47:20 +0300

    add file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * insert_nodeint_at_index - function with 3 arguments
@@ -9,39 +11,39 @@
  * Description: inserts a new node at a given position
  * Return: address of the new node
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *cursor;
-	unsigned int count = 0;
+listint_t *temp, *cursor;
+unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
+if (head == NULL)
+return (NULL);
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->n = n;
-	cursor = *head;
+temp = malloc(sizeof(listint_t));
+if (temp == NULL)
+return (NULL);
+temp->n = n;
+cursor = *head;
 
-	if (idx == 0)
-	{
-		temp->next = *head;
-		*head = temp;
-		return (*head);
-	}
+if (idx == 0)
+{
+temp->next = *head;
+*head = temp;
+return (*head);
+}
 
-	while (cursor != NULL)
-	{
-		if (count == idx - 1)
-		{
-			temp->next = cursor->next;
-			cursor->next = temp;
-		}
-		count++;
-		cursor = cursor->next;
-	}
-	if (idx > count)
-		return (NULL);
-	return (temp);
-
+while (cursor != NULL)
+{
+if (count == idx - 1)
+{
+temp->next = cursor->next;
+cursor->next = temp;
+}
+count++;
+cursor = cursor->next;
+}
+if (idx > count)
+return (NULL);
+return (temp);
 }
