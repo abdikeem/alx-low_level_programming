7bfa3792a100bf4857cbb16d136d9919644ff97f
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-26 21:07:16 +0300

    add file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,22 +8,22 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+size_t num = 0;
+long int diff;
 
-	while (head)
-	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
+while (head)
+{
+diff = head - head->next;
+num++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (diff > 0)
+head = head->next;
+else
+{
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
+}
+}
 
-	return (num);
+return (num);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,51 +1,38 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
-* free_listint_safe - frees a list
-* @h: the pointer to a pointer of head
-*
-* Description: does stuff. i didnt get this right
-*	this is similar to the last problem, but now we
-*	want to free instead of print. inside the second loop
-*	pretty much the same code to iterate through, just free it all
-* Return: the size of the list
-*/
-
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t **array;
-	unsigned int i = 0;
-	unsigned int flag = 0;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-	array = malloc(sizeof(listint_t *) * 1024);
-	if (!array)
-		exit(98);
-	while (*h != NULL)
-	{
-		for (i = 0; i < count; i++)
-		{
-			if (*h == array[i])
-			{
-				flag = 1;
-				break;
-			}
-			else
-				flag = 0;
-		}
-		if (flag == 1)
-			break;
-		array[count] = *h;
-		*h = (*h)->next;
-		count++;
-	}
-	i = 0;
-	while (i < count)
-	{
-		free(array[i]);
-		i++;
-	}
-	free(array);
-	*h = NULL;
-	return (count);
+if (!h || !*h)
+return (0);
+
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+*h = temp;
+len++;
+}
+else
+{
+*h = NULL;
+len++;
+break;
+}
+}
+
+*h = NULL;
+
+return (len);
 }
