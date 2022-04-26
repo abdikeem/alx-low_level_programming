f9dc451a96d113d35247d4fe4b8c45f84fa72d50
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-26 20:35:13 +0300

    add file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,81 +1,41 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * delete_nodeint_at_index - deletes a nodes at a given index
- * @head: the node at the beginning of the list
- * @index: the index of the node to delete
- * Return: 1 if the deletion was successful, otherwise -1
+ * delete_nodeint_at_index - deletes the nodet at index of listint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node should be deleted
+ * Return: 1 if it succeeded and -1 if it failed
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
-
 {
-
-unsigned int i;
-
 	listint_t *node;
+	unsigned int pos;
 
-		listint_t *prev;
-
-
-
-			if (head && *head)
-
-					{
-
-			prev = NULL;
-
-										for (i = 0; i <= index; i++)
-
-													{
-
-												if (i > 0 && !prev)
-
-																						break;
-
-																				if (i == index)
-
-																						{
-
-																						if (prev && prev->next)
-
-																															{
-
-																																								node = prev->next;
-
-																																									prev->next = prev->next->next;
-
-																																																		free(node);
-																																																		return (1);
-
-																																																											}
-
-																																	if (!prev)
-
-																																						{
-
-																																												node = *head;
-
-																																																	*head = node->next;
-
-																																																						free(node);
-
-																																																											return (1);
-
-																																																															}
-
-																																				}
-
-																							prev = i == 0 ? *head : prev->next;
-
-																									}
-
-											}
-
-				return (-1);
-
+	pos = 0;
+	while (*head)
+	{
+		/* with the position we find the index we need to delete */
+		if (pos == index)
+		{
+			/* node is the one we want to delete */
+			/* that is in the index position */
+			/* so we point to that  node through *head */
+			node = (*head);
+			/* *head moves to next node */
+			(*head) = (*head)->next;
+			/* node remains in the index position and we deleted */
+			/* releasing that memory */
+			free(node);
+			/* success return 1 */
+			return (1);
+		}
+		/* if the position isn't equal to the index we're looking for */
+		/* move head to next */
+		head = &(*head)->next;
+		/* and counte position keeps incrementing */
+		pos++;
+	}
+	/* failed return -1 */
+	return (-1);
 }
-
