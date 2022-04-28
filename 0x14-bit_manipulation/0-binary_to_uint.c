de2ea872255098a3c5592e483e73eb55fb47267b
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-28 22:03:32 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int val = 0;
-int i = 0;
+	int i;
+	unsigned int val = 0;
 
-if (b == NULL)
-return (0);
+	if (!b)
+		return (0);
 
-for (; b[i]; i++)
-{
-    if (b[i] < '0' || b[i] > '1')
-    {
-        return (0);
-    }
-    val = 2 * val + (b[i] - '0');
-}
-return (val);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val = 2 * val + (b[i] - '0');
+	}
+	return (val);
 }
